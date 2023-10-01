b8ece79f64fe5336cbed93a59c50c41c53ec0ed6
Farid Gibran <farid.gibran@vostra.co.id>
2023-10-01 15:16:59 +0700

    Initial Commit

---
+++ app/src/main/java/drop/swift/sale/MainActivity.java
@@ -0,0 +1,19 @@
+package drop.swift.sale;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
