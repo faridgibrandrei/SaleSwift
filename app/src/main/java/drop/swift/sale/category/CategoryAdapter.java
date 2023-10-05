b168c28d283c926251ec3660fbfa6750e5fa7eab
Farid Gibran <farid.gibran@vostra.co.id>
2023-10-05 13:35:32 +0700

    update : - added UI/UX for category card selection

--- app/src/main/java/drop/swift/sale/category/CategoryAdapter.java
+++ app/src/main/java/drop/swift/sale/category/CategoryAdapter.java
@@ -13,7 +13,8 @@ import drop.swift.sale.R;
 import drop.swift.sale.model.CategoryModel;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryViewHolder> {
-    private List<CategoryModel> categoryList; // Replace with your data type
+    private List<CategoryModel> categoryList;
+    private int selectedItemPosition = -1; // Initially, no item is selected
 
     public CategoryAdapter(List<CategoryModel> categoryList) {
         this.categoryList = categoryList;
@@ -28,7 +29,29 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
-        String categoryName = categoryList.get(position).getName();
+        CategoryModel selectedCategory = categoryList.get(position);
+
+        // Set the CardView's selected state based on the position
+        holder.categoryContainer.setSelected(position == selectedItemPosition);
+
+        // Apply different background drawables for selected and unselected states
+        if (position == selectedItemPosition) {
+            holder.categoryContainer.setBackgroundResource(R.drawable.card_category_selected);
+        } else {
+            holder.categoryContainer.setBackgroundResource(R.drawable.card_category_unselected);
+        }
+
+        // Handle item click to update the selected item
+        holder.itemView.setOnClickListener(v -> {
+            if (selectedItemPosition != position) {
+                int previousSelected = selectedItemPosition;
+                selectedItemPosition = position;
+                notifyItemChanged(previousSelected); // Deselect the previous item
+                notifyItemChanged(position); // Select the new item
+            }
+        });
+
+        String categoryName = selectedCategory.getName();
         holder.categoryText.setText(categoryName);
     }
 
--- app/src/main/java/drop/swift/sale/category/CategoryViewHolder.java
+++ app/src/main/java/drop/swift/sale/category/CategoryViewHolder.java
@@ -4,15 +4,18 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import drop.swift.sale.R;
 
 public class CategoryViewHolder extends RecyclerView.ViewHolder {
     public TextView categoryText;
+    public CardView categoryContainer;
 
     public CategoryViewHolder(@NonNull View itemView) {
         super(itemView);
         categoryText = itemView.findViewById(R.id.tv_category);
+        categoryContainer = itemView.findViewById(R.id.category_container);
     }
 }
